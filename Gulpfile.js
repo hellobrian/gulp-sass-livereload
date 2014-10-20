b794d52303b5cc64e1d3783b36a766b8bcdb5578
Brian Han <brianhan87usa@gmail.com>
2014-10-19 21:04:28 -0500

    Complete default task to run local webserver with livereload and sass compile

--- Gulpfile.js
+++ Gulpfile.js
@@ -17,9 +17,35 @@ var server = {
   port: '3000'
 }
 
+// Compile scss files to css
 gulp.task('sass', function () {
   gulp.src( sourcePaths.styles )
     .pipe(plumber())
     .pipe(sass())
     .pipe(gulp.dest( distPaths.styles ));
 });
+
+// Run a local webserver 
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      host: server.host, 
+      port: server.port, 
+      livereload: true,
+      directoryListing: false
+    }));
+});
+
+// Automatically open browser to the server.host and port 
+gulp.task('openbrowser', function() {
+  opn( 'http://' + server.host + ':' + server.port );
+});
+
+gulp.task('watch', function() {
+  gulp.watch(sourcePaths.styles, ['sass']);
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('default', ['build', 'webserver', 'watch', 'openbrowser']);
+
