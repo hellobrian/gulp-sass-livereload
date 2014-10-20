ac9fe2941255ebd8a5f223025862fdb4aaa1ebcb
Brian Han <brianhan87usa@gmail.com>
2014-10-19 20:45:33 -0500

    Sass task is working

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,25 @@
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-var sass = require('gulp-sass');
+var gulp      = require('gulp');
+var plumber   = require('gulp-plumber');
+var sass      = require('gulp-sass');
 var webserver = require('gulp-webserver');
-var opn = require('opn');
+var opn       = require('opn');
+
+var sourcePaths = {
+  styles: ['scss/**/*.scss']
+};
+
+var distPaths = {
+  styles: 'css'
+};
+
+var server = {
+  host: 'localhost',
+  port: '3000'
+}
+
+gulp.task('sass', function () {
+  gulp.src( sourcePaths.styles )
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest( distPaths.styles ));
+});
