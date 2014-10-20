7cea3b34f09174d1d6205514475026847f05f77f
Brian Han <brianhan87usa@gmail.com>
2014-10-19 20:30:42 -0500

    npm install gulp deps

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     compass = require('gulp-compass'), 
-    liveReload = require('gulp-live-reload'),
+    liveReload = require('gulp-livereload'),
     notify = require('gulp-notify')
