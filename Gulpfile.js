2d024cb229c8a7e0229ccc937654878dfebb4363
Brian Han <brianhan87usa@gmail.com>
2014-10-19 20:25:40 -0500

    First commit, require gulp deps in gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    compass = require('gulp-compass'), 
+    liveReload = require('gulp-live-reload'),
+    notify = require('gulp-notify')
